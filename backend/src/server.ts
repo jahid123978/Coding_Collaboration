69a122a82002189a9250097beec759c91147356a
jahid123978 <jahidhasan21cse@gmail.com>
2024-12-20 16:35:31 +0600

    Coding Collaboration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,6 +16,11 @@ app.use(express.json())
 app.use(cors({ origin: "http://localhost:5173",
 	methods: ['GET', 'POST', 'PATCH', 'PUT']
  }))
+ app.use((req, res, next) => { 
+	res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   next(); });
 
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -28,8 +28,8 @@ export const useSocket = (): SocketContextType => {
 }
 
 // const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
-// const BACKEND_URL = "https://coding-collaboration-api-git-main-jahid123978s-projects.vercel.app/"
-const BACKEND_URL = "http://localhost:4000/"
+const BACKEND_URL = "https://coding-collaboration-socket-api.vercel.app/"
+// const BACKEND_URL = "http://localhost:4000/"
 
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
