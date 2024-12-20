0f90a54decadc735a9782fad691aa28ff16f3081
jahid123978 <jahidhasan21cse@gmail.com>
2024-12-20 13:08:47 +0600

    Online Coding Collaboration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "http://localhost:5173",
 	},
 	maxHttpBufferSize: 1e8,
 	pingTimeout: 60000,
